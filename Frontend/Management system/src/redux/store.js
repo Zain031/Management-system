b666f774de65e584d176de546a98ba43776a88e0
ZAINU FIRDAUS IMANSYAH <zazain031@gmail.com>
2024-11-20 21:27:38 +0700

    feat:store

--- Frontend/Management system/src/redux/store.js
+++ Frontend/Management system/src/redux/store.js
@@ -1,7 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import authSlice from "./AuthSlice";
+import foodStuffsSlice from "./FoodStuffsSlice";
+import purchaseSlice from "./PurchasesSlice";
+import drinksSlice from "./DrinksSlice";
+import profitsSlice from "./ProfitsSlice";
+import salesSlice from "./SalesSlice";
+import toolsSlice from "./ToolsSlice";
+import foodSlice from "./FoodsSlice";
+
 const store = configureStore({
-    reducer: {},
+    reducer: {
+        auth: authSlice.reducer,
+        foodStuffs: foodStuffsSlice.reducer,
+        purchases: purchaseSlice.reducer,
+        drinks: drinksSlice.reducer,
+        profits: profitsSlice.reducer,
+        sales: salesSlice.reducer,
+        tools: toolsSlice.reducer,
+        foods: foodSlice.reducer,
+    },
 });
 
 export default store;
